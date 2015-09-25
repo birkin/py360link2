b7047d291d3a35d61bb65eca349f57b829b5fc0a
birkin <birkin.diana@gmail.com>
2015-09-25 13:28:46 -0400

    updates dir names.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from setuptools import setup, find_packages
 
 
-setup(name='py360link',
+setup(name='py360link-v2',
     version='2',
     packages = find_packages(),
     test_suite = 'py360link.test'
