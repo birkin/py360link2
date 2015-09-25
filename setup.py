d9c34009dea9c43941da1e6250bda948128d8b5c
birkin <birkin.diana@gmail.com>
2015-09-25 10:12:17 -0400

    bumps version.

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
-#import os
-#import ez_setup
-#ez_setup.use_setuptools()
-
 from setuptools import setup, find_packages
 
 setup(name='py360link',
-    version='1',
+    version='2',
     packages = find_packages(),
     test_suite = 'py360link.test'
 )
