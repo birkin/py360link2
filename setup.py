752ff18a27df2c6107e5a876542855f10f6a2f78
birkin <birkin.diana@gmail.com>
2019-07-15 16:27:41 -0400

    updates version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='py360link2',
-    version='3',
+    version='4',  # for python3 compatible version
     packages = find_packages(),
     test_suite = 'py360link2.test'
 )
